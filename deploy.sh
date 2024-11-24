a9dbe25739ea5babad63a3fa411167cc7e9e89e5
Sam James <sam@samdjames.uk>
2024-11-24 22:41:05 +0000

    ops(deploy): reload nginx on deploy && move redirects file

--- deploy.sh
+++ deploy.sh
@@ -66,6 +66,9 @@ fi
 echo "[+] Deploying to remote"
 rsync -e "ssh -p $REMOTE_PORT" -azv --delete ./public/ "$REMOTE_HOST:$REMOTE_PATH"
 
+echo "[+] Reload nginx to apply any rewrites"
+ssh -p$REMOTE_PORT $REMOTE_HOST "sudo /opt/reload_nginx.sh"
+
 echo "[+] Flush varnish cache for domain"
 ssh -p$REMOTE_PORT $REMOTE_HOST "sudo /opt/flush_varnish.sh docs.sdj.pw"
 
